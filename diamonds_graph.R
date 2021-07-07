af95ed6df879dc71be4fb58768c619d17265c550
hwolff00 <hwolff00@gmail.com>
2021-07-07 12:12:14 -0400

    Diamond bar chart and filtering

--- diamonds_graph.R
+++ diamonds_graph.R
@@ -1,11 +1,17 @@
 # Create a facet_wrap set of scatterplots using ggplot and the tidyverse 
 # diamonds dataset
+library(dplyr)
+library(tidyverse)
+library(janitor)
 
-head(diamonds)
 as_tibble(diamonds)
-ggplot(data = diamonds, aes(x = carat, y = price, color = cut)) + 
-  geom_point() +
-  facet_wrap(~cut)
 
+# Filter diamonds
+d<- diamonds %>%
+  filter(cut == "Ideal" & price >= 6000)
 
+# Stacked bar chart
+ggplot(data = d) +
+  geom_bar(aes(carat, fill=color)) +
+  labs(title= "$6000+ Diamond Carats")
 
